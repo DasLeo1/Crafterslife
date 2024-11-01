2153c88b465237c7e631df151482b02178b162ac
Das_leo_ <lenny14032005@gmail.com>
2024-11-01 23:28:58 +0100

    Crafterslife try / catch update

--- src/main/java/de/crafterslife/crafterslife/Crafterslife.java
+++ src/main/java/de/crafterslife/crafterslife/Crafterslife.java
@@ -2,18 +2,28 @@ package de.crafterslife.crafterslife;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.logging.Level;
+
 public final class Crafterslife extends JavaPlugin {
 
 
     @Override
     public void onEnable() {
-        getLogger().info("Crafterslife is enabled!");
-
         String prefix = "Crafterslife";
         // Plugin startup logic
 
         new Display().display(getLogger() , prefix);
 
+
+
+
+
+
+        try {
+            getLogger().log(Level.INFO, "All System`s loaded");   //Alles in bester ordnung
+        } catch (Exception exception) {
+            getLogger().log(Level.WARNING, "loading onEnable has failed", exception);  //Plugin kann nicht geladen werden , gibt fehler aus
+        }
     }
 
     @Override
